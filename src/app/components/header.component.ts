6efd0cf6d7afa7cf78f8be4fa436601cf197e4df
Cristian <cristianalemanfuentes@gmail.com>
2017-08-06 21:13:17 -0500

    Header inicio

---
+++ src/app/components/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+
+
+  <h1>Este es el header Component</h1>
+  
+  `
+})
+export class HeaderComponent {
+  
+}
